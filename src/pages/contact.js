23f9b99ff1341854c3454a2db66e65520b3f50bc
Julian Acosta <julian.acosta@alwaysai.co>
2021-08-31 20:46:49 -0700

    remove unused import and add norefferrer to a tag

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Footer from "../components/footer"
 
 import Layout from "../components/layout"
 
@@ -9,7 +8,7 @@ const Contact = () => {
       <h1>Contact</h1>
       <p>
         Feel free to contact me at{" "}
-        <a href="https://www.google.com" target="_blank">
+        <a href="https://www.google.com" target="_blank" rel="noreferrer">
           julian.acosta@email.com
         </a>
         .
