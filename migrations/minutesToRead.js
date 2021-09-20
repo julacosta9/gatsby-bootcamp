afb975f4f13731e3d9314ae08c1c21f1f3a8cb03
Julian Acosta <julian.acosta@alwaysai.co>
2021-09-20 00:56:40 -0400

    remove comments

--- migrations/minutesToRead.js
+++ migrations/minutesToRead.js
@@ -1,5 +1,4 @@
 module.exports = function (migration) {
-  // Create a new category field in the blog post content type.
   const blogPost = migration.editContentType('blogPost');
   blogPost.createField('minutesToRead', {
     name: 'Minutes to Read',
